3100d0166efa77c340457e1860d406e8f8711007
Mateus Paiva <matteusp@alu.ufc.br>
2023-03-16 15:55:27 -0300

    alteração questão 5

--- Questao5.py
+++ Questao5.py
@@ -1,3 +1,7 @@
 texto = input("Digite uma string: ")
-texto_invertido = texto[::-1]
+
+texto_invertido = ""
+for i in range(len(texto) - 1, -1, -1):
+    texto_invertido += texto[i]
+
 print(texto_invertido)
